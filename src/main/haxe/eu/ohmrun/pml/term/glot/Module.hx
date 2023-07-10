99e230c2e34a63eac5737225d235b502d83bb8f6
0b1kn00b <polysemantic@gmail.com>
2023-07-10 16:26:44 +0200

    feat: glot.toPml

--- src/main/haxe/eu/ohmrun/pml/term/glot/Module.hx
+++ src/main/haxe/eu/ohmrun/pml/term/glot/Module.hx
@@ -3,14 +3,6 @@ package eu.ohmrun.pml.term.glot;
 using eu.ohmrun.Glot;
 
 import eu.ohmrun.pml.term.glot.encode.GExpr;
-import eu.ohmrun.pml.term.glot.encode.GConstant;
-import eu.ohmrun.pml.term.glot.encode.GBinop;
-import eu.ohmrun.pml.term.glot.encode.GEFieldKind;
-import eu.ohmrun.pml.term.glot.encode.GTypePath;
-import eu.ohmrun.pml.term.glot.encode.GTypeParam;
-import eu.ohmrun.pml.term.glot.encode.GObjectField;
-import eu.ohmrun.pml.term.glot.encode.GQuoteStatus;
-import eu.ohmrun.pml.term.glot.encode.GUnop;
 import eu.ohmrun.pml.term.glot.encode.EnumValue;
 import eu.ohmrun.pml.term.glot.encode.Dyn;
 import eu.ohmrun.pml.term.glot.encode.Object;
@@ -26,30 +18,6 @@ private class Encode extends Clazz{
   private function get_GExpr():GExpr{
     return __.option(this.GExpr).def(() -> this.GExpr = new GExpr());
   }
-  @:isVar public var GConstant(get,null):GConstant;
-  private function get_GConstant():GConstant{
-    return __.option(this.GConstant).def(() -> this.GConstant = new GConstant());
-  }
-  @:isVar public var GBinop(get,null):GBinop;
-  private function get_GBinop():GBinop{
-    return __.option(this.GBinop).def(() -> this.GBinop = new GBinop());
-  }
-  @:isVar public var GEFieldKind(get,null):GEFieldKind;
-  private function get_GEFieldKind():GEFieldKind{
-    return __.option(this.GEFieldKind).def(() -> this.GEFieldKind = new GEFieldKind());
-  }
-  @:isVar public var GQuoteStatus(get,null):GQuoteStatus;
-  private function get_GQuoteStatus():GQuoteStatus{
-    return __.option(this.GQuoteStatus).def(() -> this.GQuoteStatus = new GQuoteStatus());
-  }
-  @:isVar public var GObjectField(get,null):GObjectField;
-  private function get_GObjectField():GObjectField{
-    return __.option(this.GObjectField).def(() -> this.GObjectField = new GObjectField());
-  }
-  @:isVar public var GUnop(get,null):GUnop;
-  private function get_GUnop():GUnop{
-    return __.option(this.GUnop).def(() -> this.GUnop = new GUnop());
-  }
   @:isVar public var EnumValue(get,null):EnumValue;
   private function get_EnumValue():EnumValue{
     return __.option(this.EnumValue).def(() -> this.EnumValue = new EnumValue());
@@ -61,13 +29,5 @@ private class Encode extends Clazz{
   @:isVar public var Object(get,null):Object;
   private function get_Object():Object{
     return __.option(this.Object).def(() -> this.Object = new Object());
-  }
-  @:isVar public var GTypePath(get,null):GTypePath;
-  private function get_GTypePath():GTypePath{
-    return __.option(this.GTypePath).def(() -> this.GTypePath = new GTypePath());
-  }
-  @:isVar public var GTypeParam(get,null):GTypeParam;
-  private function get_GTypeParam():GTypeParam{
-    return __.option(this.GTypeParam).def(() -> this.GTypeParam = new GTypeParam());
   }
 }
--- src/main/haxe/eu/ohmrun/pml/term/glot/encode/GBinop.hx
+++
@@ -1,9 +0,0 @@
-package eu.ohmrun.pml.term.glot.encode;
-
-import eu.ohmrun.glot.expr.GBinop in TGBinop;
-
-class GBinop extends Clazz{
-  public function apply(self:TGBinop){
-    return PValue(AnSym(self.canonical()));
-  }
-}
